22c8d52588c6a787987c9e9257ac8d9464d5e1fc
Stacey Lai <staceyclai@gmail.com>
2024-02-06 17:12:50 -0800

    Python Testing Lab
    TDD Lab from https://johnxu21.github.io/teaching/CS472/Timetable/dynamic_analysis/?

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,49 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status 
+
+class CounterTest(TestCase):
+    """Counter tests"""
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        client = app.test_client()
+        result = client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should update a counter by 1"""
+        result = self.client.post("/counters/boo")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(result.json['boo'], 0)
+
+        originalValue = result.json['boo']
+        updated = self.client.put("/counters/boo")
+        self.assertEqual(originalValue + 1, updated.json['boo'])
+
+    def test_read_a_counter(self):
+        """It should read the value of a counter"""
+        result = self.client.post("/counters/foobar")
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(result.json['foobar'], 0) # inital starting count
+
+        readValue = self.client.get("/counters/foobar")
+        self.assertEqual(0, readValue.json['foobar'])
